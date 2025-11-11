315bb637d8855d0f5bb9585cf3f039c7509b9191
abdulkader3 <kader.bd.developer@gmail.com>
2025-11-11 11:04:43 +0600

    Enhance Navbar responsiveness and update PrimaryButton styles

--- app/Components/Navbar/Navber.jsx
+++ app/Components/Navbar/Navber.jsx
@@ -1,8 +1,10 @@
+'use client'
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../../../public/photos/Logo.png'
 import Link from 'next/link';
 import PrimaryButton from '../Ui/PrimaryButton'
+import { FaBars } from 'react-icons/fa';
 
 
 
@@ -10,16 +12,24 @@ import PrimaryButton from '../Ui/PrimaryButton'
 
 
 const Navber = () => {
+
+  const [navButton , setNavButton] = useState(false)
+
+  const ButtonClictForNavber = ()=>{
+    setNavButton(true)
+    
+  }
+
   return (
     <>
 
-    <nav className='w-full h-[81px] flex justify-between items-center px-[150px] ' >
+    <nav className='w-full h-[40px] md:h-[81px] flex justify-center md:justify-between items-center px-[150px] ' >
 
-      <div className="Logo w-[119px] h-[40px]  ">
+      <div className="Logo w-[50px] h-[18px] md:w-[119px] md:h-[40px]  ">
         <Image src={Logo} alt='Logo' className='w-full h-full'/>
       </div>
 
-      <ul className='flex gap-[30px] '>
+      <ul className=' hidden md:block md:flex gap-0 md:gap-[30px] '>
 
         <li className=' hover:text-[#2E7D32] transition-all '>
           <Link href={"/"}> Home </Link>
@@ -44,6 +54,40 @@ const Navber = () => {
       </ul>
 
       <PrimaryButton Title={"Get a Quote"}/>
+
+      <div className=" block md:hidden">
+
+        <div className="">
+          <div onClick={ButtonClictForNavber} className=""><FaBars/></div>
+
+           <ul className=''>
+
+        <li className=' hover:text-[#2E7D32] transition-all '>
+          <Link href={"/"}> Home </Link>
+        </li>
+
+        <li className=' hover:text-[#2E7D32] transition-all '>
+          <Link href={"/about"}> About </Link>
+        </li>
+
+        <li className=' hover:text-[#2E7D32] transition-all '>
+          <Link href={"/services"}> Services </Link>
+        </li>
+
+        <li className=' hover:text-[#2E7D32] transition-all '>
+          <Link href={"/projects"}> Projects </Link>
+        </li>
+
+        <li className=' hover:text-[#2E7D32] transition-all '>
+          <Link href={"/contact"}> Contact </Link>
+        </li>
+
+      </ul>
+
+
+        </div>
+
+      </div>
 
     </nav>
       
--- app/Components/Ui/PrimaryButton.jsx
+++ app/Components/Ui/PrimaryButton.jsx
@@ -6,7 +6,7 @@ const PrimaryButton = ({
   return (
     <>
 
-    <button className='py-[8px] px-[24px] bg-[#2E7D32] rounded-[20px] text-white ' > {Title} </button>
+    <button className=' py-[2px] md:py-[8px]  px-[10px] md:px-[24px] bg-[#2E7D32] rounded-[20px] text-white ' > {Title} </button>
       
     </>
   );
