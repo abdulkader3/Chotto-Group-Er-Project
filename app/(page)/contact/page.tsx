42ea75ee3128b5485febf8e80ed8f33ad158a3d0
silma subah <silmasubah105@gmail.com>
2025-11-13 16:21:12 +0600

    responsive_baki

--- app/(page)/contact/page.tsx
+++ app/(page)/contact/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Banner from "@/app/Components/contact/Banner"
 import Message from "@/app/Components/contact/Message"
-
+import Location from "@/app/Components/contact/Location"
+import Start from "@/app/Components/contact/Start"
 const page = () => {
   return (
     <>
@@ -9,7 +10,8 @@ const page = () => {
 
     <Banner/>
     <Message/>
-      
+    <Location/>
+    <Start/>
 
 
 
